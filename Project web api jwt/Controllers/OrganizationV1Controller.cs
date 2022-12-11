fb743682bd02ffa908c073cafe430b1ab244e3e8
praveenrio24 <praveenrio4315@gmail.com>
2022-12-11 11:37:28 -0600

    new

--- Project web api jwt/Controllers/OrganizationV1Controller.cs
+++ Project web api jwt/Controllers/OrganizationV1Controller.cs
@@ -14,58 +14,56 @@ using System.Collections;
 
 namespace Project_web_api_jwt.Controllers
 {
-   [Authorize]
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class OrganizationV1Controller : ControllerBase
     {
         private readonly IConfiguration _configuration;
         private readonly IJwtTokenGeneration _jwtTokenGeneration;
-        public OrganizationV1Controller(IConfiguration configuration,IJwtTokenGeneration jwtTokenGeneration)
+        public OrganizationV1Controller(IConfiguration configuration, IJwtTokenGeneration jwtTokenGeneration)
         {
             _configuration = configuration;
         }
         [HttpPost]
         [Route("GetOrganization")]
-        public USP_Praveen_Organization_FetchAll Login(int OrganizationId,string OrganizationName)
-
-           {
-            SqlConnection con = new SqlConnection(_configuration.GetConnectionString("SQLCONNECT"));
-            Result result = new Result();
-         
-            tbl_Praveen_Employee tbl = new tbl_Praveen_Employee();
-            Hashtable hashtable = new Hashtable();
-            Hashtable hashtable1 = new Hashtable();
-            DateTime StartDate = DateTime.UtcNow;
-            try
-            {
-
-                con.Open();
-                SqlCommand cmd = new SqlCommand("USP_Praveen_Organization_FetchAll", con);
-                cmd.CommandType = CommandType.StoredProcedure;
-                cmd.Parameters.Add(new SqlParameter("@OrganizationId", OrganizationId));
-                cmd.Parameters.Add(new SqlParameter("@OrganizationName", OrganizationName));
-
-                IDataReader idr = cmd.ExecuteReader();
-                USP_Praveen_Organization_FetchAll datatable = new USP_Praveen_Organization_FetchAll();
-                datatable = DataReaderMapToObject<USP_Praveen_Organization_FetchAll>(idr);
-                return datatable;
-            }
-            catch(Exception ex)
-            {
-                throw (ex);
-            }
-        
-               
-
-
-        }
-
-        private T DataReaderMapToObject<T>(IDataReader idr)
+        public List<USP_Praveen_Organization_FetchAll> GetList(int OrganizationId, string OrganizationName)
         {
-            throw new NotImplementedException();
+            SqlConnection connection = new SqlConnection(_configuration.GetConnectionString("SQLCONNECT"));
+            connection.Open();
+            USP_Praveen_Organization_FetchAll all = new USP_Praveen_Organization_FetchAll();
+            string procedureName = "USP_Praveen_Organization_FetchAll";
+            var result = new List<USP_Praveen_Organization_FetchAll>();
+            using (SqlCommand command = new SqlCommand(procedureName, connection))
+            {
+                command.CommandType = CommandType.StoredProcedure;
+                command.Parameters.Add(new SqlParameter("@OrganizationId",all.OrganizationID));
+                command.Parameters.Add(new SqlParameter("@OrganizationName",all.OrganizationName));
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        int id = int.Parse(reader[0].ToString());
+                        string name = reader[1].ToString();
+                        float? age = float.Parse(reader[2]?.ToString());
+                        string Country = reader[3].ToString();
+                        float? savings = float.Parse(reader[4]?.ToString());
+                        USP_Praveen_Organization_FetchAll tmpRecord = new USP_Praveen_Organization_FetchAll()
+                        {
+                            OrganizationID = OrganizationId,
+                            OrganizationName = OrganizationName,
+
+
+                        };
+                        result.Add(tmpRecord);
+                    }
+                }
+            }
+            return result;
+
         }
     }
-
-   
 }
+              
+
+         
